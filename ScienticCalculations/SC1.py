93e2489acfdcbeae8aa77bd2f7569880586a123a
jacob rodrigo <jacobR3301@github.io>
2024-04-11 18:03:04 -0400

    Finished problem

--- ScienticCalculations/SC1.py
+++ ScienticCalculations/SC1.py
@@ -1,4 +1,17 @@
 ### Squared circle (25 points) ###
+import random
 
 def solve(rayon, L, nbrPoints):
-    pass
+    circHits = 0
+    print(L, " ", rayon)
+    for pointNum in range(nbrPoints):
+        #( random.random(), L)
+        xPos = random.random()*L/2
+        yPos = random.random()*L/2
+        
+        dist = (xPos ** 2 + yPos ** 2) ** 0.5
+        print(xPos, yPos, dist)
+        if (xPos ** 2 + yPos ** 2) ** 0.5 <= rayon:
+            circHits += 1
+    #print(circHits/nbrPoints)
+    return(circHits/nbrPoints)
