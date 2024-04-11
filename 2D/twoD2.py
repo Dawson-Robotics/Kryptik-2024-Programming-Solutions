6266f8c01ed05bb776260184c2caa4a914a12eca
Safin Haque <safinhq13@gmail.com>
2024-04-11 18:58:40 -0400

    finished twoD1

--- 2D/twoD2.py
+++ 2D/twoD2.py
@@ -1,4 +1,5 @@
 ### 2D2 Connect the dots (30 points) ###
+import re
 
 def solve(board):
     pass
@@ -8,6 +9,13 @@ def solve(board):
 ### helper function to visualize the board ###
 def print_board(board):
     for line in board:
+        xcounter=0
         for el in line:
-            print(el, end=' ')
-        print()
+            if(el == 'x'):
+                xcounter= xcounter+1
+            #print(el, end=' ')
+        if(xcounter == 2):
+            line = re.sub("^x.*x$", '-', line)
+            print(line)
+
+print_board(["x......x"])
